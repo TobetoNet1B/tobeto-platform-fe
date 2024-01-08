1aed22599dac15aa88138087c22f78cfe8ed0189
enesttn <yenestutun@gmail.com>
2024-01-08 22:46:04 +0300

    --

--- src/components/Routes/RouteDefinitions.tsx
+++ src/components/Routes/RouteDefinitions.tsx
@@ -1,6 +1,5 @@
 import Homepage from "pages/Homepage/Homepage";
 import NotFound from "pages/NotFound";
-import Platform from "pages/Platform/Platform";
 import SignInPage from "pages/SignInPage/SignInPage";
 import SignUpPage from "pages/SignUpPage/SignUpPage";
 import React from "react";
@@ -15,7 +14,6 @@ const RouteDefinitions = (props: Props) => {
 			<Route path="*" element={<NotFound />} />
 			<Route path="/giris" element={<SignInPage />} />
 			<Route path="/kayit-ol" element={<SignUpPage />} />
-			<Route path="/platform" element={<Platform/>}></Route>
 		</Routes>
 	);
 };
