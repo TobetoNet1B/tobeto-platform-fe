3e0db5525f1333f250ed7a6a2575e44b8486ac64
tnhnyldrm <46500254+tnhnyldrm@users.noreply.github.com>
2024-01-14 14:30:35 +0300

    changes in education life

--- src/pages/MyProfile/MyProfile.tsx
+++ src/pages/MyProfile/MyProfile.tsx
@@ -334,15 +334,22 @@ const MyProfile = (props: Props) => {
 										Eğitim Hayatım ve Deneyimlerim
 									</h5>
 									<hr className="border-violet-300 border-b-2 " />
-									<div className='p'>
-										<ul className="timeline timeline-vertical lg:timeline-horizontal">
+										<ul className="timeline timeline-vertical ">
+											<li>
+												<div className="timeline-start">1984</div>
+												<div className="timeline-middle">
+													<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+												</div>
+												<div className="timeline-end timeline-box">Tobeto Lisesi</div>
+												<hr />
+											</li>
 											<li>
 												<hr />
 												<div className="timeline-start">1998</div>
 												<div className="timeline-middle">
 													<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
 												</div>
-												<div className="timeline-end timeline-box">University</div>
+												<div className="timeline-end timeline-box">Tobeto University</div>
 												<hr />
 											</li>
 											<li>
@@ -351,7 +358,7 @@ const MyProfile = (props: Props) => {
 												<div className="timeline-middle">
 													<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
 												</div>
-												<div className="timeline-end timeline-box">Company</div>
+												<div className="timeline-end timeline-box">Tobeto Şirketi</div>
 												<hr />
 											</li>
 											<li>
@@ -360,7 +367,7 @@ const MyProfile = (props: Props) => {
 												<div className="timeline-middle">
 													<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
 												</div>
-												<div className="timeline-end timeline-box">Tobeto</div>
+												<div className="timeline-end timeline-box">Tobeto Tech Company</div>
 												<hr />
 											</li>
 											<li>
@@ -369,10 +376,9 @@ const MyProfile = (props: Props) => {
 												<div className="timeline-middle">
 													<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
 												</div>
-												<div className="timeline-end timeline-box">Tech Company</div>
+												<div className="timeline-end timeline-box">Tobeto Yüksek Lisans</div>
 											</li>
 										</ul>
-									</div>
 								</Card>
 							</div>
 						</div>
