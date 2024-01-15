694ace6f6f6d6c6c192c6e61cc1276fa587e6528
Emirhan Çelebi <emirhan@celebi.dev>
2024-01-15 20:44:52 +0300

    added redux

---
+++ src/store/Auth/AuthSlice.ts
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+	name: "auth",
+	initialState: {
+		isLoggedIn: false
+	},
+	reducers: {
+		
+	}
+})
+
+export const authReducer = authSlice.reducer;
