2c0ddb0a58198ab3acde3d20c04905d794a56acc
Emirhan Çelebi <emirhan@celebi.dev>
2024-01-19 03:35:52 +0300

    PersonalInformations

--- src/components/ProfileInfo/PersonalInformations/index.tsx
+++ src/components/ProfileInfo/PersonalInformations/index.tsx
@@ -1,4 +1,6 @@
+import { log } from 'console';
 import { Datepicker } from 'flowbite-react';
+import './personal-informations.css'
 import { useFormik } from 'formik'
 
 type Props = {}
@@ -10,7 +12,7 @@ const PersonalInformations = (props: Props) => {
 			name: '',
 			surname: '',
 			phone: '',
-			birthdate: '',
+			birthDate: '',
 			identityNumber: '',
 			email: '',
 			country: '',
@@ -25,27 +27,27 @@ const PersonalInformations = (props: Props) => {
 
 	return (
 		<form onSubmit={formik.handleSubmit} className=''>
-			<div className='grid grid-cols-12 gap-y-4 gap-x-3 mx-auto'>
+			<div className='grid grid-cols-12 gap-6 mx-auto'>
 				<div className="avatar col-span-12">
-					<div className="w-40 rounded-full mx-auto">
+					<div className="w-36 rounded-full mx-auto">
 						<img src="https://tobeto.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimages.19a45d39.png&w=48&q=75" />
 					</div>
 				</div>
-				<div className='col-span-6'>
+				<div className='lg:col-span-6 col-span-12'>
 					<label htmlFor='name'>Adınız</label>
 					<input id='name' type='text' name='name' onChange={formik.handleChange} value={formik.values.name}
 						className="input input-bordered w-full" />
 				</div>
-				<div className='col-span-6'>
+				<div className='lg:col-span-6 col-span-12'>
 					<label htmlFor='surname'>Soyadınız</label>
 					<input id='surname' type='text' name='surname' onChange={formik.handleChange} value={formik.values.surname}
 						className="input input-bordered w-full" />
 				</div>
-				<div className='col-span-6'>
+				<div className='lg:col-span-6 col-span-12'>
 					<label htmlFor='tel'>Telefon Numaranız</label>
 					<div className='join w-full'>
-						<select className="select select-bordered join-item">
-							<option selected>+90</option>
+						<select defaultValue="+90" className="select select-bordered join-item">
+							<option>+90</option>
 							<option>+1</option>
 							<option>+44</option>
 						</select>
@@ -53,37 +55,44 @@ const PersonalInformations = (props: Props) => {
 							className="input input-bordered w-full join-item" />
 					</div>
 				</div>
-				<div className='col-span-6'>
+				<div className='lg:col-span-6 col-span-12'>
 					<label htmlFor='birthDate'>Doğum Tarihiniz</label>
-					<Datepicker language="tr-TR" labelTodayButton="Bugün" labelClearButton="Temizle" maxDate={new Date()} weekStart={1}
-						className='input input-bordered w-full pt-0.5 pl-0' style={{ backgroundColor: "transparent", border: "none" }} />
+					<input id='birthDate' type='date' name='birthDate' max={new Date().toLocaleDateString('sv-SE')}
+						onChange={formik.handleChange} value={formik.values.birthDate}
+						className="input input-bordered w-full" />
+				</div>
+				<div className='lg:col-span-6 col-span-12'>
+					<label htmlFor='identityNumber'>TC Kimlik No</label>
+					<input id='identityNumber' type='number' name='identityNumber' onChange={formik.handleChange} value={formik.values.identityNumber}
+						className="input input-bordered w-full" />
+				</div>
+				<div className='lg:col-span-6 col-span-12'>
+					<label htmlFor='email'>E-posta</label>
+					<input id='email' type='email' name='email' onChange={formik.handleChange} value={formik.values.email}
+						className="input input-bordered w-full" />
 				</div>
 				<div className='col-span-12'>
-
+					<label htmlFor='country'>Ülke</label>
+					<input id='country' type='text' name='country' onChange={formik.handleChange} value={formik.values.country}
+						className="input input-bordered w-full" />
 				</div>
-				<div className='col-span-6'>
-
+				<div className='lg:col-span-6 col-span-12'>
+					<label htmlFor='city'>Şehir</label>
+					<input id='city' type='text' name='city' onChange={formik.handleChange} value={formik.values.city}
+						className="input input-bordered w-full" />
 				</div>
-				<div className='col-span-6'>
-
+				<div className='lg:col-span-6 col-span-12'>
+					<label htmlFor='district'>İlçe</label>
+					<input id='district' type='text' name='district' onChange={formik.handleChange} value={formik.values.district}
+						className="input input-bordered w-full" />
 				</div>
 				<div className='col-span-12'>
-
-				</div>
-				<div className='col-span-12'>
-
+					<label htmlFor='addressDetails'>Adres</label>
+					<textarea id='addressDetails' name='addressDetails' onChange={formik.handleChange} value={formik.values.district}
+						className="textarea textarea-bordered w-full"></textarea>
 				</div>
 			</div>
-
-			{/* <label htmlFor="email">Email Address</label>
-			<input
-				id="email"
-				name="email"
-				type="email"
-				onChange={formik.handleChange}
-				value={formik.values.email}
-			/> */}
-			<button type="submit" className='btn'>Submit</button>
+			<button type="submit" className='btn mt-5'>Submit</button>
 		</form>
 	)
 }
