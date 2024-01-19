eaa66334d0eb5a8d216770515ea02a9c4a9be18f
Emirhan Çelebi <emirhan@celebi.dev>
2024-01-19 03:35:27 +0300

    update ProfileInfo

--- src/pages/ProfileInfo/index.tsx
+++ src/pages/ProfileInfo/index.tsx
@@ -6,10 +6,10 @@ type Props = {}
 const ProfileInfo = (props: Props) => {
 	return (
 		<div className='grid grid-cols-12 container lg:max-w-6xl mx-auto lg:pt-40 pt-20'>
-			<div className='lg:col-span-4 col-span-12 mx-auto md:max-w-full max-w-sm overflow-x-auto'>
+			<div className='lg:col-span-3 col-span-12 md:max-w-full max-w-sm mx-0 sm:mx-auto lg:mx-0 overflow-x-auto'>
 				<SideBar />
 			</div>
-			<div className='lg:col-span-8 col-span-12 ml-10 lg:mt-0 mt-10'>
+			<div className='lg:col-span-9 col-span-12 lg:mt-0 mt-10'>
 				<Outlet />
 			</div>
 		</div>
