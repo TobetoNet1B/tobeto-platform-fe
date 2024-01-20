49809dbe6ca75cb2d344750cf9a3275abffa8fae
Emirhan Çelebi <emirhan@celebi.dev>
2024-01-20 22:47:13 +0300

    fixes and improvements on profile page. added CityAndDistrictTR.json file.

--- src/components/ProfileInfo/PersonalInformations/index.tsx
+++ src/components/ProfileInfo/PersonalInformations/index.tsx
@@ -1,11 +1,15 @@
-import { log } from 'console';
-import { Datepicker } from 'flowbite-react';
 import './personal-informations.css'
 import { useFormik } from 'formik'
+import { MdOutlineEdit } from "react-icons/md";
+import { useEffect, useState } from 'react'
+import axios from 'axios';
+import CityAndDistrictTR from 'utils/CityAndDistrictTR.json'
 
 type Props = {}
 
 const PersonalInformations = (props: Props) => {
+
+	const [city, setCity] = useState([])
 
 	const formik = useFormik({
 		initialValues: {
@@ -16,9 +20,10 @@ const PersonalInformations = (props: Props) => {
 			identityNumber: '',
 			email: '',
 			country: '',
-			city: '',
+			city: 'SAKARYA',
 			district: '',
-			addressDetails: ''
+			addressDetails: '',
+			about: ''
 		},
 		onSubmit: values => {
 			alert(JSON.stringify(values, null, 2));
@@ -26,25 +31,26 @@ const PersonalInformations = (props: Props) => {
 	});
 
 	return (
-		<form onSubmit={formik.handleSubmit} className=''>
+		<form onSubmit={formik.handleSubmit} className='flex-shrink-0 flex-grow-0'>
 			<div className='grid grid-cols-12 gap-6 mx-auto'>
 				<div className="avatar col-span-12">
 					<div className="w-36 rounded-full mx-auto">
 						<img src="https://tobeto.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimages.19a45d39.png&w=48&q=75" />
+						<MdOutlineEdit className='z-10 absolute ml-24 top-24 bg-white text-violet-600 rounded-full text-3xl p-2' />
 					</div>
 				</div>
 				<div className='lg:col-span-6 col-span-12'>
-					<label htmlFor='name'>Adınız</label>
+					<label htmlFor='name'>Adınız*</label>
 					<input id='name' type='text' name='name' onChange={formik.handleChange} value={formik.values.name}
 						className="input input-bordered w-full" />
 				</div>
 				<div className='lg:col-span-6 col-span-12'>
-					<label htmlFor='surname'>Soyadınız</label>
+					<label htmlFor='surname'>Soyadınız*</label>
 					<input id='surname' type='text' name='surname' onChange={formik.handleChange} value={formik.values.surname}
 						className="input input-bordered w-full" />
 				</div>
 				<div className='lg:col-span-6 col-span-12'>
-					<label htmlFor='tel'>Telefon Numaranız</label>
+					<label htmlFor='tel'>Telefon Numaranız*</label>
 					<div className='join w-full'>
 						<select defaultValue="+90" className="select select-bordered join-item">
 							<option>+90</option>
@@ -52,19 +58,20 @@ const PersonalInformations = (props: Props) => {
 							<option>+44</option>
 						</select>
 						<input id='tel' type='tel' name='tel' onChange={formik.handleChange} value={formik.values.phone}
-							className="input input-bordered w-full join-item" />
+							className="input input-bordered w-full join-item" placeholder='5** *** ** **' />
 					</div>
 				</div>
 				<div className='lg:col-span-6 col-span-12'>
-					<label htmlFor='birthDate'>Doğum Tarihiniz</label>
+					<label htmlFor='birthDate'>Doğum Tarihiniz*</label>
 					<input id='birthDate' type='date' name='birthDate' max={new Date().toLocaleDateString('sv-SE')}
 						onChange={formik.handleChange} value={formik.values.birthDate}
 						className="input input-bordered w-full" />
 				</div>
 				<div className='lg:col-span-6 col-span-12'>
-					<label htmlFor='identityNumber'>TC Kimlik No</label>
+					<label htmlFor='identityNumber'>TC Kimlik No*</label>
 					<input id='identityNumber' type='number' name='identityNumber' onChange={formik.handleChange} value={formik.values.identityNumber}
 						className="input input-bordered w-full" />
+					<span className='italic font-extralight text-sm text-red-500'>*Aboneliklerde fatura için doldurulması zorunlu alan</span>
 				</div>
 				<div className='lg:col-span-6 col-span-12'>
 					<label htmlFor='email'>E-posta</label>
@@ -72,27 +79,36 @@ const PersonalInformations = (props: Props) => {
 						className="input input-bordered w-full" />
 				</div>
 				<div className='col-span-12'>
-					<label htmlFor='country'>Ülke</label>
+					<label htmlFor='country'>Ülke*</label>
 					<input id='country' type='text' name='country' onChange={formik.handleChange} value={formik.values.country}
 						className="input input-bordered w-full" />
 				</div>
 				<div className='lg:col-span-6 col-span-12'>
-					<label htmlFor='city'>Şehir</label>
-					<input id='city' type='text' name='city' onChange={formik.handleChange} value={formik.values.city}
-						className="input input-bordered w-full" />
+					<label htmlFor='city'>İl*</label>
+					<select className='select select-bordered w-full'
+						id='city' name='city' onChange={formik.handleChange} value={formik.values.city}>
+						{CityAndDistrictTR.map(c => (
+							<option key={c.value} >{c.city}</option>
+						))}
+					</select>
 				</div>
 				<div className='lg:col-span-6 col-span-12'>
-					<label htmlFor='district'>İlçe</label>
-					<input id='district' type='text' name='district' onChange={formik.handleChange} value={formik.values.district}
+					<label htmlFor='district'>İlçe*</label>
+					<input disabled id='district' type='text' name='district' onChange={formik.handleChange} value={formik.values.district}
 						className="input input-bordered w-full" />
 				</div>
 				<div className='col-span-12'>
 					<label htmlFor='addressDetails'>Adres</label>
-					<textarea id='addressDetails' name='addressDetails' onChange={formik.handleChange} value={formik.values.district}
-						className="textarea textarea-bordered w-full"></textarea>
+					<textarea id='addressDetails' name='addressDetails' onChange={formik.handleChange} value={formik.values.addressDetails}
+						className="textarea textarea-bordered h-28 w-full"></textarea>
+				</div>
+				<div className='col-span-12'>
+					<label htmlFor='about'>Hakkımda</label>
+					<textarea id='about' name='about' onChange={formik.handleChange} value={formik.values.about}
+						className="textarea textarea-bordered h-32 w-full" placeholder='Kendini kısaca tanıt'></textarea>
 				</div>
 			</div>
-			<button type="submit" className='btn mt-5'>Submit</button>
+			<button type="submit" className='btn btn-md px-5 mt-5 rounded-full bg-[#93f] hover:bg-[#822bd9] text-white'>Kaydet</button>
 		</form>
 	)
 }
--- src/pages/ProfileInfo/index.tsx
+++ src/pages/ProfileInfo/index.tsx
@@ -5,7 +5,7 @@ type Props = {}
 
 const ProfileInfo = (props: Props) => {
 	return (
-		<div className='grid grid-cols-12 container lg:max-w-6xl mx-auto lg:pt-40 pt-20'>
+		<div className='grid grid-cols-12 container lg:max-w-6xl min-h-svh mx-auto my-20 text-gray-500 font-medium'>
 			<div className='lg:col-span-3 col-span-12 md:max-w-full max-w-sm mx-0 sm:mx-auto lg:mx-0 overflow-x-auto'>
 				<SideBar />
 			</div>
