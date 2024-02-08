5fe215e9733b05afb4ea6e8646cfb1ef50aa715f
Emirhan Çelebi <emirhan@celebi.dev>
2024-02-08 06:30:43 +0300

    added tobeto colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,137 +1,249 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./node_modules/flowbite-react/lib/esm/**/*.js",
-    "./node_modules/tw-elements-react/dist/js/**/*.js",
-  ],
-  plugins: [
-    require("flowbite/plugin"),
-    require("daisyui"),
-    require("tw-elements-react/dist/plugin.cjs"),
-  ],
-  daisyui: {
-    themes: ["light"],
-  },
-  theme: {
-    extend: {
-      keyframes: {
-        fade: {
-          "0%": { opacity: 0 },
-          "20%": { opacity: 1 },
-          "33.333%": { opacity: 1 },
-          "53.333%": { opacity: 0 },
-        },
-        fill: {
-          "0% ": { width: "0" },
-          "100%": { width: "100%" },
-        },
-        animateHeart: {
-          "40%": { transform: "scale(1.2)" },
-          "100%": { transform: "scale(1)" },
-        },
-        animateHeartOut: {
-          "0%": { transform: "scale(1.4)" },
-          "100%": { transform: "scale(1)" },
-        },
-        animateCircle: {
-          "40%": {
-            transform: "scale(10)",
-            opacity: 1,
-            fill: "#dd4688",
-          },
-          "55%": {
-            transform: "scale(11)",
-            opacity: 1,
-            fill: "#d46abf",
-          },
-          "65%": {
-            transform: "scale(12)",
-            opacity: 1,
-            fill: "#cc8ef5",
-          },
-          "75%": {
-            transform: "scale(13)",
-            opacity: 1,
-            fill: "transparent",
-            stroke: "#cc8ef5",
-            strokeWidth: ".5",
-          },
-          "85%": {
-            transform: "scale(17)",
-            opacity: 1,
-            fill: "transparent",
-            stroke: "#cc8ef5",
-            strokeWidth: ".2",
-          },
-          "95%": {
-            transform: "scale(18)",
-            opacity: 1,
-            fill: "transparent",
-            stroke: "#cc8ef5",
-            strokeWidth: ".1",
-          },
-          "100%": {
-            transform: "scale(19)",
-            opacity: 1,
-            fill: "transparent",
-            stroke: "#cc8ef5",
-            strokeWidth: "0",
-          },
-        },
-        antProgresActive: {
-          "0%": {
-            transform: "translateX(-100%) scaleX(0)",
-            opacity: 0.1,
-          },
-          "20%": {
-            transform: "translateX(-100%) scaleX(0)",
-            opacity: 0.5,
-          },
-          "100%": {
-            transform: "translateX(0) scaleX(1)",
-            opacity: 0,
-          },
-        },
-        preloaderAnimation: {
-          "0%": {
-            opacity: 0.3,
-          },
-          "50%": {
-            opacity: 1,
-          },
-          "100%": {
-            opacity: 0.3,
-          }
-        },
-        carrosel: {
-          "0%": {
-            transform: "rotateY(1turn) translateZ(130px) rotateY(-1turn)",
-            zIndex: 10,
-            opacity: 1
-          },
-        
-          "50%": {
-            zIndex: -10
-          },
-        
-          "to": {
-            transform: "rotateY(0deg) translateZ(130px) rotateY(0deg)",
-            zIndex: 10,
-            opacity: 1,
-          }
-        }
-      },
-    },
-    animation: {
-      fade: "fade 4s ease infinite",
-      fill: "fill 10s linear 1",
-      animateHeart: "animateHeart 0.3s linear forwards 0.25s",
-      animateHeartOut: "animateHeartOut 0.3s linear forwards",
-      animateCircle: "animateCircle 0.3s linear forwards",
-      antProgresActive: "antProgresActive 2.4s cubic-bezier(0.23, 1, 0.32, 1) infinite",
-      preloaderAnimation: "preloaderAnimation 2s linear infinite",
-      carrosel: "carrosel 12s linear infinite"
-    } /*  fade 4s ease 0s infinite normal none running fade auto normal normal */,
-  },
+	content: [
+		"./src/**/*.{js,jsx,ts,tsx}",
+		"./node_modules/flowbite-react/lib/esm/**/*.js",
+		"./node_modules/tw-elements-react/dist/js/**/*.js",
+	],
+	plugins: [
+		require("flowbite/plugin"),
+		require("daisyui"),
+		require("tw-elements-react/dist/plugin.cjs"),
+	],
+	daisyui: {
+		themes: ["light"],
+	},
+	theme: {
+		extend: {
+			colors: {
+				bs_blue: "#56a2bd",
+				bs_indigo: "#6610f2",
+				bs_purple: "#6f42c1",
+				bs_pink: "#d63384",
+				bs_red: "#fc5c46",
+				bs_orange: "#fd7e14",
+				bs_yellow: "#fbdc8e",
+				bs_green: "#006251",
+				bs_teal: "#20c997",
+				bs_cyan: "#0dcaf0",
+				bs_black: "#000",
+				bs_white: "#fff",
+				bs_gray: "#57534e",
+				bs_gray_dark: "#292524",
+				bs_gray_100: "#f5f5f4",
+				bs_gray_200: "#e7e5e4",
+				bs_gray_300: "#d6d3d1",
+				bs_gray_400: "#a8a29e",
+				bs_gray_500: "#78716c",
+				bs_gray_600: "#57534e",
+				bs_gray_700: "#44403c",
+				bs_gray_800: "#292524",
+				bs_gray_900: "#1c1917",
+				bs_primary: "#93f",
+				bs_secondary: "#93f",
+				bs_success: "#65b996",
+				bs_info: "#4d4d4d",
+				bs_warning: "#ed9c56",
+				bs_danger: "#fc5c46",
+				bs_light: "#f5f5f4",
+				bs_dark: "#181717",
+				bs_primary_light: "#e1e1e1",
+				bs_secondary_light: "#ebe3ff",
+				bs_success_light: "#e6efee",
+				bs_info_light: "#4d4d4d",
+				bs_warning_light: "#fef8e8",
+				bs_danger_light: "#ffefec",
+				bs_light_light: "#fafaf9",
+				bs_primary_rgb: "153,51,255",
+				bs_secondary_rgb: "153,51,255",
+				bs_success_rgb: "101,185,150",
+				bs_info_rgb: "77,77,77",
+				bs_warning_rgb: "237,156,86",
+				bs_danger_rgb: "252,92,70",
+				bs_light_rgb: "245,245,244",
+				bs_dark_rgb: "24,23,23",
+				bs_primary_light_rgb: "225,225,225",
+				bs_secondary_light_rgb: "235,227,255",
+				bs_success_light_rgb: "230,239,238",
+				bs_info_light_rgb: "77,77,77",
+				bs_warning_light_rgb: "254,248,232",
+				bs_danger_light_rgb: "255,239,236",
+				bs_light_light_rgb: "250,250,249",
+				bs_primary_text_emphasis: "#3d1466",
+				bs_secondary_text_emphasis: "#3d1466",
+				bs_success_text_emphasis: "#284a3c",
+				bs_info_text_emphasis: "#1f1f1f",
+				bs_warning_text_emphasis: "#5f3e22",
+				bs_danger_text_emphasis: "#65251c",
+				bs_light_text_emphasis: "#44403c",
+				bs_dark_text_emphasis: "#44403c",
+				bs_primary_bg_subtle: "#ebd6ff",
+				bs_secondary_bg_subtle: "#ebd6ff",
+				bs_success_bg_subtle: "#e0f1ea",
+				bs_info_bg_subtle: "#dbdbdb",
+				bs_warning_bg_subtle: "#fbebdd",
+				bs_danger_bg_subtle: "#fededa",
+				bs_light_bg_subtle: "#fafafa",
+				bs_dark_bg_subtle: "#a8a29e",
+				bs_primary_border_subtle: "#d6adff",
+				bs_secondary_border_subtle: "#d6adff",
+				bs_success_border_subtle: "#c1e3d5",
+				bs_info_border_subtle: "#b8b8b8",
+				bs_warning_border_subtle: "#f8d7bb",
+				bs_danger_border_subtle: "#febeb5",
+				bs_light_border_subtle: "#e7e5e4",
+				bs_dark_border_subtle: "#78716c",
+				bs_white_rgb: "255,255,255",
+				bs_black_rgb: "0,0,0",
+				bs_body_color: "#1c1917",
+				bs_body_color_rgb: "28,25,23",
+				bs_body_bg: "#fff",
+				bs_body_bg_rgb: "255,255,255",
+				bs_emphasis_color: "#000",
+				bs_emphasis_color_rgb: "0,0,0",
+				bs_secondary_color: "rgba(28,25,23,.75)",
+				bs_secondary_color_rgb: "28,25,23",
+				bs_secondary_bg: "#e7e5e4",
+				bs_secondary_bg_rgb: "231,229,228",
+				bs_tertiary_color: "rgba(28,25,23,.5)",
+				bs_tertiary_color_rgb: "28,25,23",
+				bs_tertiary_bg: "#f5f5f4",
+				bs_tertiary_bg_rgb: "245,245,244",
+				bs_heading_color: "#001410",
+				bs_link_color: "#93f",
+				bs_link_color_rgb: "153,51,255",
+				bs_link_decoration: "underline",
+				bs_link_hover_color: "#7a29cc",
+				bs_link_hover_color_rgb: "122,41,204",
+				bs_code_color: "#d63384",
+				bs_highlight_bg: "#fef5dd",
+				bs_border_width: "1px",
+				bs_border_style: "solid",
+				bs_border_color: "#f5f5f4",
+				bs_border_color_translucent: "rgba(0,0,0,.175)",
+				bs_focus_ring_color: "rgba(153,51,255,.25)",
+				bs_form_valid_color: "#65b996",
+				bs_form_valid_border_color: "#65b996",
+				bs_form_invalid_color: "#fc5c46",
+				bs_form_invalid_border_color: "#fc5c46",
+			},
+			keyframes: {
+				fade: {
+					"0%": { opacity: 0 },
+					"20%": { opacity: 1 },
+					"33.333%": { opacity: 1 },
+					"53.333%": { opacity: 0 },
+				},
+				fill: {
+					"0% ": { width: "0" },
+					"100%": { width: "100%" },
+				},
+				animateHeart: {
+					"40%": { transform: "scale(1.2)" },
+					"100%": { transform: "scale(1)" },
+				},
+				animateHeartOut: {
+					"0%": { transform: "scale(1.4)" },
+					"100%": { transform: "scale(1)" },
+				},
+				animateCircle: {
+					"40%": {
+						transform: "scale(10)",
+						opacity: 1,
+						fill: "#dd4688",
+					},
+					"55%": {
+						transform: "scale(11)",
+						opacity: 1,
+						fill: "#d46abf",
+					},
+					"65%": {
+						transform: "scale(12)",
+						opacity: 1,
+						fill: "#cc8ef5",
+					},
+					"75%": {
+						transform: "scale(13)",
+						opacity: 1,
+						fill: "transparent",
+						stroke: "#cc8ef5",
+						strokeWidth: ".5",
+					},
+					"85%": {
+						transform: "scale(17)",
+						opacity: 1,
+						fill: "transparent",
+						stroke: "#cc8ef5",
+						strokeWidth: ".2",
+					},
+					"95%": {
+						transform: "scale(18)",
+						opacity: 1,
+						fill: "transparent",
+						stroke: "#cc8ef5",
+						strokeWidth: ".1",
+					},
+					"100%": {
+						transform: "scale(19)",
+						opacity: 1,
+						fill: "transparent",
+						stroke: "#cc8ef5",
+						strokeWidth: "0",
+					},
+				},
+				antProgresActive: {
+					"0%": {
+						transform: "translateX(-100%) scaleX(0)",
+						opacity: 0.1,
+					},
+					"20%": {
+						transform: "translateX(-100%) scaleX(0)",
+						opacity: 0.5,
+					},
+					"100%": {
+						transform: "translateX(0) scaleX(1)",
+						opacity: 0,
+					},
+				},
+				preloaderAnimation: {
+					"0%": {
+						opacity: 0.3,
+					},
+					"50%": {
+						opacity: 1,
+					},
+					"100%": {
+						opacity: 0.3,
+					}
+				},
+				carrosel: {
+					"0%": {
+						transform: "rotateY(1turn) translateZ(130px) rotateY(-1turn)",
+						zIndex: 10,
+						opacity: 1
+					},
+
+					"50%": {
+						zIndex: -10
+					},
+
+					"to": {
+						transform: "rotateY(0deg) translateZ(130px) rotateY(0deg)",
+						zIndex: 10,
+						opacity: 1,
+					}
+				}
+			},
+		},
+		animation: {
+			fade: "fade 4s ease infinite",
+			fill: "fill 10s linear 1",
+			animateHeart: "animateHeart 0.3s linear forwards 0.25s",
+			animateHeartOut: "animateHeartOut 0.3s linear forwards",
+			animateCircle: "animateCircle 0.3s linear forwards",
+			antProgresActive: "antProgresActive 2.4s cubic-bezier(0.23, 1, 0.32, 1) infinite",
+			preloaderAnimation: "preloaderAnimation 2s linear infinite",
+			carrosel: "carrosel 12s linear infinite"
+		} /*  fade 4s ease 0s infinite normal none running fade auto normal normal */,
+	},
 };
