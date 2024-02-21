bd7136f055b7ceaeb9d9c352a5c32659d4550a94
Emirhan Çelebi <emirhan@celebi.dev>
2024-02-21 16:16:35 +0300

    calendar page - sidebar

---
+++ src/pages/Calendar/index.tsx
@@ -0,0 +1,19 @@
+import Calendar from "./Calendar"
+import Sidebar from "./Sidebar"
+
+type Props = {}
+
+const CalendarPage = (props: Props) => {
+	return (
+		<div className="grid grid-cols-12 my-5">
+			<div className='lg:col-span-3 col-span-12'>
+				<Sidebar />
+			</div>
+			<div className='lg:col-span-9 col-span-12'>
+				<Calendar />
+			</div>
+		</div>
+	)
+}
+
+export default CalendarPage
