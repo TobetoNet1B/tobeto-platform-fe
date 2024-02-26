8a0ddd86a2c8736b1e62112740678b3c37f698c4
tnhnyldrm <46500254+tnhnyldrm@users.noreply.github.com>
2024-02-26 19:38:19 +0300

    platform education added

--- src/components/Platform/Main/EducationShowcase.tsx
+++ src/components/Platform/Main/EducationShowcase.tsx
@@ -1,43 +1,57 @@
+import { GetStudentClassroomResponse } from "models/responses/student-classrooms/getStudentClassroomResponse";
+import React, { useEffect, useState } from "react";
+import studentClassroomService from "services/studentClassroomService";
 import Card, { ECard } from "utils/Platform/Card";
 
 type CardData = {
-  image?: string;
+  imgUrl?: string;
   description?: string;
-  tarih: string;
+  createdDate: string;
   buttonText?: string;
-  kurum?:string
-  name:string;
+  companyName?: string
+  name: string;
+  id?: string;
 };
 
 type Props = {
   searchQuery: string;
   sortBy: string;
-  selectedOrganization:string;
+  selectedOrganization: string;
 };
 
 const EducationShowcase = (props: Props) => {
 
-  const cardData: CardData[] = [
-    { image: "https://www.allotraining.com/wp-content/uploads/2021/08/c-.net_.png",kurum:"İstanbul Kodluyor", name: ".NET & REACT FULLSTACK 1-B", tarih: "2023-09-21 15:20", buttonText: "Eğitime Git" },
-    { image: "https://www.allotraining.com/wp-content/uploads/2021/08/c-.net_.png", name: "JAVA & REACT FULLSTACK 1-B", tarih: "2023-09-28 15:20", buttonText: "Eğitime Git" },
-    { image: "https://www.allotraining.com/wp-content/uploads/2021/08/c-.net_.png",kurum:"İstanbul Kodluyor",  name: "Springboot & REACT FULLSTACK", tarih: "2023-09-21 10:20", buttonText: "Eğitime Git" },
-    { image: "https://www.allotraining.com/wp-content/uploads/2021/08/c-.net_.png",kurum:"İstanbul Kodluyor",  name: ".NET & React Fullstack | Öğrenme Yolculuğu", tarih: "2023-09-21 15:20", buttonText: "Eğitime Git" },
-  ];
+  const [studentModule, setStudentModule] = useState<GetStudentClassroomResponse | null>(null);
+
+  useEffect(() => {
+    const fetchStudentModule = async () => {
+      try {
+        const result = await studentClassroomService.getById(localStorage.studentId);
+        setStudentModule(result.data as GetStudentClassroomResponse);
+        console.log(result.data)
+      } catch (error) {
+        console.error("Error fetching student module:", error);
+      }
+    };
+
+    fetchStudentModule();
+  }, []);
+
   const filterCards = (card: CardData) => {
     return (
       (!props.selectedOrganization ||
-        (card.kurum === "İstanbul Kodluyor" &&
+        (card.companyName === "İstanbul Kodluyor" &&
           props.selectedOrganization === "kurum1")) &&
       (props.searchQuery === "" ||
         card.name.toLowerCase().includes(props.searchQuery.toLowerCase()))
-  
+
     );
   };
 
   const sortCards = (a: CardData, b: CardData) => {
-    const dateA = new Date(a.tarih).getTime();
-    const dateB = new Date(b.tarih).getTime();
-  
+    const dateA = new Date(a.createdDate).getTime();
+    const dateB = new Date(b.createdDate).getTime();
+
     switch (props.sortBy) {
       case "name-asc":
         return a.name.localeCompare(b.name);
@@ -51,30 +65,37 @@ const EducationShowcase = (props: Props) => {
         return 0;
     }
   };
-
-  const filteredAndSortedCards = cardData
-  .filter(filterCards)
-  .sort(sortCards);
-
+  const filteredAndSortedCards = studentModule && studentModule.moduleSets
+    ? studentModule.moduleSets.map(module => ({
+      imgUrl: module.imgUrl,
+      name: module.name,
+      createdDate: module.classroomStartDate.toString(),
+      buttonText: "Eğitime Git",
+      companyName: module.companyName,
+      id: module.id
+    })).filter(filterCards).sort(sortCards)
+    : [];
+  console.log("filteredAndSortedCards:", filteredAndSortedCards);
   return (
     <div className="flex flex-wrap justify-center">
-    {filteredAndSortedCards.length > 0 ? (
-      filteredAndSortedCards.map((card, index) => (
-        <ECard
-          key={index}
-          imgUrl={card.image}
-          name={card.name}
-          companyName={card.kurum}
-          createdDate={card.tarih}
-          buttonText={card.buttonText}
-        />
-      ))
-    ) : (
-      <p className="text-center text-gray-500">
-        Size atanan herhangi bir eğitim bulunmamaktadır.
-      </p>
-    )}
-  </div>
+      {filteredAndSortedCards.length > 0 ? (
+        filteredAndSortedCards.slice(0, 4).map((card, index) => (
+          <ECard
+            key={index}
+            imgUrl={card.imgUrl}
+            name={card.name}
+            companyName={card.companyName}
+            createdDate={card.createdDate}
+            buttonText={card.buttonText}
+            id={card.id}
+          />
+        ))
+      ) : (
+        <p className="text-center text-gray-500">
+          Size atanan herhangi bir eğitim bulunmamaktadır.
+        </p>
+      )}
+    </div>
   );
 };
 export default EducationShowcase;
