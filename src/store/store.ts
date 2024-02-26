9767e7b7588f5cf7c1c4a9c27f3d62a163437c3a
Emirhan Çelebi <emirhan@celebi.dev>
2024-02-26 17:11:57 +0300

    userSlice

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,9 +2,11 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "./Auth/AuthSlice";
 import { moduleSetReducer } from "./moduleSet/moduleSetSlice";
 import { studentLessonReducer } from "./studentLesson/studentLessonSlice";
+import { userReducer } from "./user/userSlice";
 
 const rootReducer = combineReducers({
 	auth: authReducer,
+	user: userReducer,
 	moduleSets: moduleSetReducer,
 	studentLessons: studentLessonReducer,
 });
