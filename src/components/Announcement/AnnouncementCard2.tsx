534384371a1473e49229a7347ae5067a7a4f5ebf
RabiaKandemir <119038074+RabiaKandemir@users.noreply.github.com>
2024-03-01 13:39:14 +0300

    ..

--- src/components/Announcement/AnnouncementCard2.tsx
+++ src/components/Announcement/AnnouncementCard2.tsx
@@ -79,21 +79,6 @@ const AnnouncementCard2 = (props: Props) => {
   const endIndex = Math.min(startIndex + cardsPerPage, filteredAndSortedCards.length);
   const paginatedCards = filteredAndSortedCards.slice(startIndex, endIndex);
 
-  const handlePageChange = (page: number) => {
-    setCurrentPage(page);
-  };
-
-  const handleNextPage = () => {
-    if (currentPage < totalPages) {
-      setCurrentPage(currentPage + 1);
-    }
-  };
-
-  const handlePrevPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-    }
-  };
 
   return (
     <div >
@@ -116,58 +101,6 @@ const AnnouncementCard2 = (props: Props) => {
           </p>
         )}
       </div>
-      {totalPages > 1 && (
-        <div className="flex justify-center mt-4 mb-2">
-          <button
-            onClick={handlePrevPage}
-            className={`px-3 py-2 mx-1 rounded-full `}
-          >
-            <svg
-              className="h-5 w-5 text-black"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              strokeWidth="2"
-              stroke="currentColor"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" />
-              <path d="M9 13l-4 -4l4 -4m-4 4h11a4 4 0 0 1 0 8h-1" />
-            </svg>
-          </button>
-          {Array.from({ length: totalPages }).map((_, page) => (
-            <button
-              key={page}
-              onClick={() => handlePageChange(page + 1)}
-              className={`px-3 py-1 mx-1 rounded-md ${currentPage === page + 1 ? 'bg-[#01956E] text-white' : 'bg-white text-black'
-                }`}
-            >
-              {page + 1}
-            </button>
-          ))}
-          <button
-            onClick={handleNextPage}
-            className={`px-2 mx-1 rounded-s`}
-          >
-            <svg
-              className="h-5 w-5 text-black"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              strokeWidth="2"
-              stroke="currentColor"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" />
-              <path d="M15 13l4 -4l-4 -4m4 4h-11a4 4 0 0 0 0 8h1" />
-            </svg>
-          </button>
-        </div>
-      )}
     </div>
   );
 };
